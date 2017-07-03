3752fbe828c4419649a677790c85c2efe3eecebf
copykad <tangzabyoc@hotmail.com>
2017-07-03 16:07:34 +0700

    Update index.php

--- .gitignore/index.php
+++ .gitignore/index.php
@@ -12,11 +12,12 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"||$arrJson['events'][0]['message']['text'] == "ว่าไง"
+   ||$arrJson['events'][0]['message']['text'] == "hi"||$arrJson['events'][0]['message']['text'] == "hello"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+  $arrPostData['messages'][0]['text'] = "สวัสดีฉันคือ copykadBot";
 }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
